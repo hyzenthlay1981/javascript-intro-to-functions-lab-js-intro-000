eae8fff80625040e59dcb5dba8186d89036a6604
JP Luttrell <hyzenthlay12@yahoo.com>
2019-11-26 19:41:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,3 +20,10 @@ function sayHiToGrandma(string) {
     return "I can\'t hear you!"
   }
 }
+
+function sayHiToGrandma(string) {
+  var uppercase = "HELLO"
+    if(string.toUpperCase === uppercase) {
+      return "YES INDEED!"
+    }
+}
